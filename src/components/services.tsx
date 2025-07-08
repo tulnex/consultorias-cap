56ac9ac9053a3f7644f8a0a36724d7952e62fb02
sebastian lozano <cesarloco2003@hotmail.com>
2025-07-08 17:39:33 -0500

    cambios

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -23,12 +23,17 @@ import ServiceCard from './service-card'
         <ServiceCard handleClick={() => setShowModal(true)} service={{name: 'P&C', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}} />
           {
             showModal &&
-            <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white'>
-              <button onClick={() => setShowModal(false)} className="bg-primary py-2 px-4 rounded-full p-4 transition-all duration-500 hover:bg-slate-800 hover:text-white">
-                x
-              </button>
-              <h1 className='text-center text-2xl font-bold'>{services.title}</h1>
-              <p className='text-center text-lg'>{services.description}</p>
+            // <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white h-[60vh] aspect-video'>
+            <div className='fixed top-[76px] left-0 w-screen h-[calc(100vh-76px)] bg-black/50 flex justify-center items-center fadein'>
+              <div className='relative w-[80vw] h-[calc(80vh-76px)] bg-white rounded-xl p-4 shadow-2xl'>
+                <div className='flex justify-end'>
+                  <button onClick={() => setShowModal(false)} className="bg-primary py-2 px-4 rounded-full p-4 transition-all duration-500 hover:bg-slate-800 hover:text-white">
+                    x
+                  </button>
+                </div>
+                <h1 className='text-center text-2xl font-bold'>{services.title}</h1>
+                <p className='text-center text-lg'>{services.description}</p>
+              </div>
             </div>
           }
     </section>
