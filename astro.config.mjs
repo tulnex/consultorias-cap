73cd0bfd79fbe16a8f8afa530228a7867beb46d8
sebastian lozano <cesarloco2003@hotmail.com>
2025-07-15 17:02:55 -0500

    cambio x

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
       plugins: [tailwindcss()]
-	},
+    },
 
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
